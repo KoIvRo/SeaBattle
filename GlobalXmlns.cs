27eb9eb8dcf37c1c5653f69ad74f51be09ada84e
unknown <korot4ev29@gmail.com>
2025-11-24 18:54:30 +1000

    init commit

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SeaBattle")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SeaBattle.Pages")]
